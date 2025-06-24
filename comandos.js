69af766da2546023f13916a33437ac4979de1e70
kliffor <kliffor@gmail.com>
2025-06-24 10:51:06 -0300

    Quarto Commit

--- comandos.js
+++ comandos.js
@@ -3,12 +3,17 @@
 const button = document.querySelector(".btn");
 const input = document.querySelector(".value");
 const p_value = document.querySelector(".p_value");
+const cur_dolar = document.querySelector(".cur_dolar");
 
+const dolar = 5.25;
 
+function convert() {
+    
+   p_value.innerHTML = input.value;
+   cur_dolar.innerHTML = input.value * dolar;
+   new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(dolar);
 
-function click() {
-    p_value.innerHTML = input.value;
-
+    
 }
 
-button.addEventListener("click", click);
+button.addEventListener("click", convert);
